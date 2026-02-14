fa421561c136100edea470b156aa7fb34a222b5f
Sakhaika <sakha00119@gmail.com>
2026-02-14 23:15:03 +0700

    belajar

---
+++ 2_TipeData/src/pkg2_tipedata/Main.java
@@ -0,0 +1,26 @@
+package pkg2_tipedata;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Tipe Data
+        
+        Angka
+            integer         -> int      -> Bilangan bulat
+            floating point  -> float    -> Bilangan berkoma
+            double          -> double   -> Bilangan berkoma
+        Text
+            string          -> kata/kalimat (Saya sedang makan)
+            char            -> karakter (A) (B) (+) (-)
+        Boolean
+            boolean         -> true/false
+        
+        Variable
+        */
+        // tipe data nama_variable = isi_variable;
+        // tipe data nama_variable;
+        
+        int umur = 22;
+        System.out.println(umur + 5);
+    }
+}
---
+++ 5_exploreVariable/src/pkg5_explorevariable/Main.java
@@ -0,0 +1,17 @@
+package pkg5_explorevariable;
+public class Main {
+    public static void main(String[] args) {
+        // integer (int)
+        int umur = 20;
+        System.out.println("Umur = " + umur + " tahun");
+        
+        // floating point (float)
+        float berat = (float) 75.9;
+        System.out.println("Berat = " + berat + "Kg");
+        
+        // double floating point (double)
+        double tinggi = 176.8;
+        System.out.println("Tinggi = " + tinggi + "cm");
+    }
+    
+}
