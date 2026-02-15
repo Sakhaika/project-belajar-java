2c19c90e93ab24751f478c01e5a40797d10fffb0
Sakhaika <sakha00119@gmail.com>
2026-02-15 21:55:00 +0700

    belajar

--- 8_InputData/src/pkg8_inputdata/Main.java
+++ 8_InputData/src/pkg8_inputdata/Main.java
@@ -3,7 +3,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int umur;
+        String nama;
+        String alamat;
+        double berat;
+        double tinggi;
+        Scanner input = new Scanner(System.in);
         
+        //Inputan data
+        System.out.println("Selamat data di program inputan");
+        System.out.print("Masukan nama anda \t= ");
+        nama = input.nextLine();
+        System.out.print("Masukan umur anda \t= ");
+        umur = input.nextInt();
+        System.out.print("Masukan tinggi anda \t= ");
+        tinggi = input.nextDouble();
+        System.out.print("Masukan berat anda \t= ");
+        berat = input.nextDouble();
+        System.out.print("Masukan alamat anda \t= ");
+        input.nextLine();
+        alamat = input.nextLine();
+        System.out.println("\n==========================");
+        
+        //Menampilkan data
+        System.out.println("Nama \t: " + nama);
+        System.out.println("Umur \t: " + umur);
+        System.out.println("Tinggi \t: " + tinggi);
+        System.out.println("Berat \t: " + berat);
+        System.out.println("Alamat \t: " + alamat);     
     }
-    
 }
