e3ff57d74fc21623571b83b1918db17932e8304d
Sakhaika <sakha00119@gmail.com>
2026-02-15 15:38:23 +0700

    belajar

---
+++ 7_input/src/pkg7_input/Main.java
@@ -0,0 +1,13 @@
+package pkg7_input;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String isi;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukan inputan anda = ");
+        isi = input.nextLine();
+        System.out.println("Inputan = " + isi);
+    } 
+}
